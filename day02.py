3358b2c0e5852b912e3d083de9ab464aaa0652dd
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2021-12-02 08:42:45 +0100

    02

--- day02.py
+++ day02.py
@@ -1,10 +1,31 @@
 def main():
-    sth = read_input("day02.txt")
+    cmds = read_input("day02.txt")
+    
+    x, y = 0, 0
+    for cmd in cmds:
+        if cmd[0] == "forward":
+            x += cmd[1]
+        elif cmd[0] == "down":
+            y += cmd[1]
+        elif cmd[0] == "up":
+            y -= cmd[1]
+    print(x, y, x * y)
+
+    x, y, aim = 0, 0, 0
+    for cmd in cmds:
+        if cmd[0] == "forward":
+            x += cmd[1]
+            y += aim * cmd[1]
+        elif cmd[0] == "down":
+            aim += cmd[1]
+        elif cmd[0] == "up":
+            aim -= cmd[1]
+    print(x, y, x * y)
 
 
 def read_input(file_name):
     with open(file_name, "r") as f:
-        return [int(x) for x in f.readlines()]
+        return [(d, int(n)) for d, n in [x.strip().split(" ") for x in f.readlines()]]
 
 
 if __name__ == "__main__":
