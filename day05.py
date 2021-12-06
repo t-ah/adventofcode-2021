cea0e8d98a73ccc4ccfd993e67f5864be94598eb
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2021-12-06 08:53:56 +0100

    day06

--- day05.py
+++ day05.py
@@ -1,23 +1,23 @@
 import re
-from collections import Counter
+from collections import defaultdict
 
 
 def main():
     lines = read_input("day05.txt")
     pattern = re.compile("([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)")
-    coordinates = [list(map(lambda x: int(x), l)) for l in [re.search(pattern, line).groups() for line in lines]]
-    print(count_overlap(coordinates, False))
-    print(count_overlap(coordinates, True))
+    coordinates = [list(map(int, l)) for l in [re.search(pattern, line).groups() for line in lines]]
+    print(count_overlap(coordinates, 2, False))
+    print(count_overlap(coordinates, 2, True))
 
 
-def count_overlap(coordinates, diagonals=True):
-    counts = Counter()
+def count_overlap(coordinates, threshold, diagonals=True):
+    counts = defaultdict(int)
     for c in coordinates:
         if c[0] == c[2] or c[1] == c[3]:
             count_line(c, counts)
         elif diagonals:
             count_diagonal(c, counts)
-    return sum(i >= 2 for i in counts.values())
+    return sum(i >= threshold for i in counts.values())
 
 
 def count_line(c, counts):
@@ -27,11 +27,11 @@ def count_line(c, counts):
 
 
 def count_diagonal(c, counts):
-    incr = 1 if c[3] > c[1] else -1
+    deltaY = 1 if c[3] > c[1] else -1
     y = c[1]
     for x in sequence(c[0], c[2]):
         counts[(x, y)] += 1
-        y += incr
+        y += deltaY
 
 
 def sequence(v1, v2):
