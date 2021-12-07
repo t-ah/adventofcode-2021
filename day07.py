35910a12961bd28ea7f18383a1ec9782c559419d
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2021-12-07 09:26:45 +0100

    optimise

--- day07.py
+++ day07.py
@@ -1,13 +1,31 @@
 def main():
     positions = read_input("day07.txt")
     positions.sort()
-    print(find_minimum(positions, False))
-    print(find_minimum(positions, True))
+    print("Result:", find_minimum(positions, False, optimised=True))
+    print("Result:", find_minimum(positions, True, optimised=True))
 
 
-def find_minimum(positions, correct):
-    fuel = [calculate_fuel(positions, i, correct) for i in range(positions[0], positions[-1] + 1)]
-    return min(fuel)
+def find_minimum(positions, correct, optimised=False):
+    if not optimised:
+        fuel = [calculate_fuel(positions, i, correct) for i in range(positions[0], positions[-1] + 1)]
+        return min(fuel)
+    else:
+        return search(len(positions) // 2, len(positions) // 2, positions, correct, 0)
+
+
+def search(index, interval, positions, correct, cnt):
+    interval = interval // 2
+    v = calculate_fuel(positions, index, correct)
+    if interval == 0:
+        print("Iterations:", cnt)
+        return v
+    v_left = calculate_fuel(positions, index - interval, correct)
+    if v_left <= v:
+        return search(index - interval, interval, positions, correct, cnt + 1)
+    v_right = calculate_fuel(positions, index + interval, correct)
+    if v_right <= v:
+        return search(index + interval, interval, positions, correct, cnt + 1)
+    return search(index, interval - 1, positions, correct, cnt + 1)
 
 
 def calculate_fuel(positions, i, correct):
