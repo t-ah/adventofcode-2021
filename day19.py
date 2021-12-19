a5ee78d0bab915cf475eed887d644ec853d395ae
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2021-12-19 20:46:12 +0100

    got it

--- day19.py
+++ day19.py
@@ -4,58 +4,48 @@ from queue import deque
 
 def main():
     scans = read_input("day19.txt")
-    scans = list(enumerate(scans))
     fixed_scans = scans[:1]
     unfixed_scans = deque(scans[1:])
     scanner_offsets = []
     
     while unfixed_scans:
         unfixed_scan = unfixed_scans.popleft()
-        fixed_scan, offsets, relative_to = compare_scans(unfixed_scan, fixed_scans)
+        fixed_scan, offsets = compare_scans(unfixed_scan, fixed_scans)
         if fixed_scan:
             fixed_scans.append(fixed_scan)
-            scanner_offsets.append((fixed_scan[0], relative_to, offsets))
+            scanner_offsets.append(offsets)
         else:
             unfixed_scans.append(unfixed_scan)
     beacons = set()
     for scan in fixed_scans:
-        for pos in scan[1]:
+        for pos in scan:
             beacons.add(tuple(pos))
     print(len(beacons))
     
-    scanner_positions = {0: (0, 0, 0)}
-    for id, relative_to_id, offset in scanner_offsets:
-        ref_pos = scanner_positions[relative_to_id]
-        ref_pos = (0,0,0) # TODO why do _all_ offsets appear to be relative to (0,0,0)?
-        scanner_positions[id] = (ref_pos[0] - offset[0], ref_pos[1] - offset[1], ref_pos[2] - offset[2])
     max_distance = 0
-    for i in range(len(scanner_positions)):
-        for j in range(i + 1, len(scanner_positions)):
-            pos1, pos2 = scanner_positions[i], scanner_positions[j]
+    for i in range(len(scanner_offsets)):
+        for j in range(i + 1, len(scanner_offsets)):
+            pos1, pos2 = scanner_offsets[i], scanner_offsets[j]
             distance = sum([abs(pos1[i] - pos2[i]) for i in range(3)])
             max_distance = max(distance, max_distance)
     print(max_distance)
 
 
 def compare_scans(unfixed_scan, fixed_scans):
-    for rotated_scan in generate_rotations(unfixed_scan[1]):
-        for fixed_i, fixed_scan in fixed_scans:
+    for rotated_scan in generate_rotations(unfixed_scan):
+        for fixed_scan in fixed_scans:
             counter = Counter()
             for pos1 in fixed_scan:
                 for pos2 in rotated_scan:
                     counter[diff(pos1, pos2)] += 1
             offsets, overlap = counter.most_common()[0]
             if overlap >= 12:
-                return (unfixed_scan[0], [[pos[0] + offsets[0], pos[1] + offsets[1], pos[2] + offsets[2]] for pos in rotated_scan]), offsets, fixed_i
-    return None, None, None
+                return [[pos[0] + offsets[0], pos[1] + offsets[1], pos[2] + offsets[2]] for pos in rotated_scan], offsets
+    return None, None
 
 
 def diff(pos1, pos2):
     return tuple([pos1[i] - pos2[i] for i in range(3)])
-
-
-def get_spacings(scan, index):
-    return [scan[i + 1][index] - scan[i][index] for i in range(len(scan) - 1)]
 
 
 def generate_rotations(positions):
